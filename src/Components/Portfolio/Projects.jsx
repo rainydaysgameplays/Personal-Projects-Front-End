dfeb17e4cfd9910ac6cb80a89691999a6c2f6b9a
rainydaysgameplays <rainydaysgameplays@gmail.com>
2022-10-05 23:10:00 -0500

    Setup

---
+++ src/Components/Portfolio/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+export default function Projects(props) {
+    return (
+        <div>
+            <Header />
+                <p>Home</p>
+            <Footer />
+        </div>
+    );
+}
